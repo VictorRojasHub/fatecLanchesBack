fd742c6da1a9b180d8fbee1a86422baf1b0b2c9a
Victor <141892245+VictorRojasHub@users.noreply.github.com>
2026-05-26 00:16:40 +0000

    frontend url

--- app.js
+++ app.js
@@ -17,8 +17,14 @@ mongoose.connect(process.env.MONGO_URI)
 const usuarioRoutes = require('./routes/usuarioRoutes');
 
 // Configuração de CORS
+//const corsOptions = {
+//  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+//  credentials: true,
+//  optionsSuccessStatus: 200
+//};
+
 const corsOptions = {
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  origin: '*', // Isso libera qualquer site para bater no seu backend
   credentials: true,
   optionsSuccessStatus: 200
 };
